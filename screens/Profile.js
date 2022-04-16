00206fc0c6ee3c32c6dd5ec84d23494ec3c3ab57
Debabrata Mondal <debabrata.js@protonmail.com>
2022-04-16 10:14:36 +0530

    update profile page

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,41 +1,87 @@
-import { Text, StyleSheet, View } from "react-native"
+import { Image, Pressable, Text, StyleSheet, View } from "react-native"
 import { StatusBar } from 'expo-status-bar';
 
+import { signOut } from "../functions/auth"
+import useUser from "../lib/user"
+
+import Loader from "../components/Loader"
+
 export default function ProfileScreen() {
-    return (
-        <View
-            style={styles.container}
-        >
-            <StatusBar style="auto" />
-            <Text
-                style={styles.text}
+    const { user } = useUser()
+
+    if (!user) {
+        return <Loader />
+    } else {
+        return (
+            <View
+                style={styles.container}
             >
-                You are signed in.
-            </Text>
-        </View>
-    )
+                <StatusBar style="auto" />
+
+                <Image
+                    source={{
+                        uri: user.user_metadata.avatar_url
+                    }}
+                    style={styles.avatar}
+                />
+
+                <Text style={styles.name}>
+                    {user.user_metadata.full_name}
+                </Text>
+
+                <Pressable
+                    style={styles.button}
+                    onPress={signOut}
+                >
+                    <Text
+                        style={styles.text}
+                    >
+                        Sign Out
+                    </Text>
+                </Pressable>
+            </View>
+        )
+    }
+
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
+        justifyContent: "flex-start",
         alignItems: "center",
         backgroundColor: "white",
     },
     button: {
         padding: 5,
-        backgroundColor: "#eaeaea",
+        marginVertical: 30,
+        backgroundColor: "#FFDFA8",
         borderRadius: 5,
-        width: 220,
+        width: 260,
+        height: 35,
         textAlign: "center",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
         flexDirection: "row"
     },
+    avatar: {
+        width: 120,
+        height: 120,
+        resizeMode: "contain",
+        marginVertical: 30,
+        borderRadius: 60,
+    },
     text: {
         textAlign: "center",
+        color: "#CB8816",
+        fontFamily: "Inter_400Regular"
     },
+    name: {
+        textAlign: "center",
+        color: "#FFC45D",
+        fontSize: 24,
+        fontFamily: "Inter_700Bold"
+    }
 })
 
